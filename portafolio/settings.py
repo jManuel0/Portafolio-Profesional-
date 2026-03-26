cb6410e6a78d4720c608402834b62bb81b133fb9
william <william.inzandara@campusucc.edu.co>
2026-03-26 09:16:25 -0500

    Hoja de vida developer3 lista

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'developer1',
+    'developer3',
 ]
 
 MIDDLEWARE = [
--- portafolio/urls.py
+++ portafolio/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('developer1/', include('developer1.urls')),
+    path('developer3/', include('developer3.urls')),
 ]
